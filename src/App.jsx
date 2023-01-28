32a68954fb8184190334f3908d45c4cea81d9d4f
Skymiles56 <120430312+Skymiles56@users.noreply.github.com>
2023-01-28 16:26:27 -0500

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
+import { useAddress, ConnectWallet } from '@thirdweb-dev/react';
+
 const App = () => {
+  const address = useAddress(); 
+  console.log("Address:", address);
+  // This is the case where the user hasn't connected their wallet
+  // to your web app. Let them call connectWallet.
+
+  if (!address) {
+    return (
+      <div classname="landing">
+        <h1>Welcome to NarutoDAO</hi>
+        <div className="btn-hero">
+          <ConnectWallet />
+          </div>
+          </div>
+         );
+        }
+          // This is the case where we have the user's address
+  // which means they've connected their wallet to our site!
+  
   return (
     <div className="landing">
-      <h1>Welcome to My DAO</h1>
-    </div>
-  );
-};
+      <h1> wallet connected, now what!</h1>
+      </div>);
+}
+
+  
 
 export default App;
