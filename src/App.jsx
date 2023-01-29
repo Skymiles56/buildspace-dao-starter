fbd31c55a618a01d048e77d13ddcb6ac0b06ffdc
Skymiles56 <120430312+Skymiles56@users.noreply.github.com>
2023-01-28 19:33:04 -0500

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,40 @@
-import { useAddress, ConnectWallet } from '@thirdweb-dev/react';
 
+ import { useAddress, ConnectWallet } from '@thirdweb-dev/react';
 const App = () => {
-  const address = useAddress(); 
-  console.log("Address:", address);
+
+
+ const App = () => {
+  // Use the hooks thirdweb give us.
+  const address = useAddress();
+  console.log("👋 Address:", address);
+
   // This is the case where the user hasn't connected their wallet
   // to your web app. Let them call connectWallet.
-
   if (!address) {
     return (
-      <div classname="landing">
-        <h1>Welcome to NarutoDAO</hi>
+      <div className="landing">
+        <h1>Welcome to NarutoDAO</h1>
         <div className="btn-hero">
           <ConnectWallet />
-          </div>
-          </div>
-         );
-        }
-          // This is the case where we have the user's address
+        </div>
+      </div>
+    );
+  }
+
+  // This is the case where we have the user's address
   // which means they've connected their wallet to our site!
-  
   return (
     <div className="landing">
-      <h1> wallet connected, now what!</h1>
-      </div>);
+      <h1>👀 wallet connected, now what!</h1>
+    </div>);
 }
 
-  
+
+  return (
+    <div className="landing">
+      <h1>Welcome to My DAO</h1>
+    </div>
+  );
+};
 
 export default App;
